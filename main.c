5956c85c0f117933da5930a5a5a588afb107cc51
miji <mijilee0413@sookmyung.ac.kr>
2023-12-07 16:14:52 +0900

    실습3

--- main.c
+++ main.c
@@ -2,33 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 
-//실습2 
+//실습3
 
-struct Book {
+struct Book { 
 	int number; 
 	char title[10]; 
 }; 
 
-int main(void) {
-	struct Book *p; 
+int main(void) 
+{
+	int i; 
+	struct Book **bookshelf; 
 	
-	p = (struct Book*)malloc(2 * sizeof (struct Book)); 
+	//동적 메모리 할당
+	bookshelf = (struct Book**)malloc(3 * sizeof (struct Book*)); 
+	for (i=0;i<3;i++) 
+		bookshelf[i] = (struct Book*)malloc(10 * sizeof (struct Book));
 
-	if (p == NULL) {
-		printf("메모리 할당 오류\n"); 
-	return 1; 
-} 
-
-	p->number = 1; 
-	strcpy(p->title, "C Programming"); 
-
-	(p + 1)->number = 2; 
-	strcpy((p + 1)->title, "Electronics"); 
+	bookshelf[1][3].number = 5; 
+	strcpy(bookshelf[1][3].title, "C++ Programming"); 
 	
-	printf("Book 1: %d %s\n", p->number, p->title);
-    printf("Book 2: %d %s\n", (p + 1)->number, (p + 1)->title);
+	(bookshelf[2]+4)->number = 3; 
+	strcpy((bookshelf[2]+4)->title, "Communications Theory"); 
+	
+	printf("book (1,3) : %i, %s\n", (bookshelf[1]+3)->number, (bookshelf[1]+3)->title); 
+	printf("book (2,4) : %i, %s\n", bookshelf[2][4].number, bookshelf[2][4].title); 
+	
+	//구조체 포인터 3개가 각각 가리키는 주소 해제
+	for (i=0;i<3;i++) 
+		free(bookshelf[i]); 
 
-	free(p); 
+    //구조체 이중 포인터가 가리키는 조소 해제
+	free(bookshelf); 
 	
 	return 0; 
-}
+} 
